0c48df559b30f0fe87558f94926cccd2e529285c
muellert-ufl <59845100+muellert-ufl@users.noreply.github.com>
2026-03-15 12:51:46 -0400

    Small expansion on the cards to add a quick match validation function for others to use, and a change suit function for when an 8 is played and set to a declared color.

--- Crazy-Eights-River-Wakes/Assets/Card.cs
+++ Crazy-Eights-River-Wakes/Assets/Card.cs
@@ -13,11 +13,10 @@ public class Card : MonoBehaviour
 
     [Tooltip("The rank (face value) for this card.\n0-9 correspond to 1-10 (face value-1), 10 to jacks, 11 to queens, and 12 to kings."), Range(0, 12)]  public int rank = 0;
 
-    private Material face_material;
+    private Material face_material; // This material corresponds to the card's face. The displayed texture is manipulated by using the shader graph and the Suit / Rank uniforms.
 
     void Start() {
         face_material = transform.GetChild(0).GetComponent<MeshRenderer>().material;
-        print(face_material.GetFloat("Suit"));
         UpdateCardFace();
     }
 
@@ -26,7 +25,22 @@ public class Card : MonoBehaviour
         face_material.SetFloat("Rank", rank);
     }
 
-    public bool IsValidMatch() {
+    public bool IsValidMatch(Card other) {
+        // 8s are wild: they always match
+        if (suit == 8) {
+            return true;
+        }
+        // Otherwise, match is valid if suit or rank matches the other card's
+        else if (suit == other.suit || rank == other.rank) {
+            return true;
+        }
         return false;
+    }
+
+    // Function for changing the suit of an 8
+    public bool ChangeSuit(Suit new_suit) {
+        suit = new_suit;
+        // TODO: Insert a special effect coroutine here (spawn smoke clouds? play a sound effect? make the card glow?)
+        UpdateCardFace();
     }
 }
