d84b1583286b759b13a75edd6e0777de517c35b9
muellert-ufl <59845100+muellert-ufl@users.noreply.github.com>
2026-03-29 19:51:37 -0400

    Now cards can be removed from hands.

--- Crazy-Eights-River-Wakes/Assets/CardHand.cs
+++ Crazy-Eights-River-Wakes/Assets/CardHand.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 using System.Collections.Generic;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
@@ -13,66 +14,90 @@ public class CardHand : MonoBehaviour
     private List<Card> heldCards;
     
     private Transform sockets;
-    private XRSocketInteractor activeSocket;
+    private XRSocketInteractor currentSocket;
 
-    private List<bool> socketStates;
+    private List<XRSocketInteractor> activeSockets;
+
+    private Stack<XRSocketInteractor> inactiveSockets;
+    
+    private List<(XRSocketInteractor, Card)> socketCardPairs;
 
     public Transform cardAnchor;
 
     void Awake()
     {
         sockets = transform.GetChild(1);
-        activeSocket = sockets.GetComponentInChildren<XRSocketInteractor>();
-
         InitializeHand();
     }
 
     private void InitializeHand()
     {
         heldCards = new List<Card>();
-        socketStates = new List<bool>();
-        
+
+        activeSockets = new List<XRSocketInteractor>();
+        inactiveSockets = new Stack<XRSocketInteractor>();
+        socketCardPairs = new List<(XRSocketInteractor, Card)>();
+
         for (int i=0; i<maxHandSize; i++)
         {
-            socketStates.Add(false);
+            var newSocket = Instantiate(socketPrefab);
+            newSocket.transform.parent = sockets;
+            newSocket.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            newSocket.transform.localPosition += new Vector3(-0.04f * i, 0.0f, 0.01f * i);
+            XRSocketInteractor interactor = newSocket.GetComponent<XRSocketInteractor>();
+            interactor.selectEntered.AddListener(delegate {AttachCardToHand(interactor);});
+            interactor.selectExited.AddListener(delegate {PopCardFromHand(interactor);});
+            inactiveSockets.Push(interactor);
+            newSocket.SetActive(false);
         }
+
+        UpdateCurrentSocket();
     }
 
-    public void AttachCardToHand()
+    public void AttachCardToHand(XRSocketInteractor eventSocket = null)
     {
         Debug.Log("Card attached to hand.");
-        var selected = activeSocket.GetOldestInteractableSelected();
+        var selected = eventSocket.GetOldestInteractableSelected();
         if (selected != null)
         {
             Card addedCard = selected.transform.gameObject.GetComponent<Card>();
             addedCard.DisablePhysics();
             heldCards.Add(addedCard);
+            socketCardPairs.Add((currentSocket, addedCard));
         }
 
-        UpdateActiveSocket();
+        UpdateCurrentSocket();
         MakeCardFan();
 
         Debug.Log("You now have " + heldCards.Count + " cards in your hand.");
     }
 
-    public void UpdateActiveSocket()
+    public void UpdateCurrentSocket()
     {
-        if (heldCards == null)
-        {
-            heldCards = new List<Card>();
-        }
-
         if (heldCards.Count < maxHandSize)
         {
-            activeSocket = sockets.GetChild(heldCards.Count).GetComponent<XRSocketInteractor>();
-            activeSocket.gameObject.SetActive(true);
-
+            XRSocketInteractor nextSocketCandidate = inactiveSockets.Pop();
+            currentSocket = nextSocketCandidate;
+            activeSockets.Add(currentSocket);
+            currentSocket.gameObject.SetActive(true);
         }
     }
 
-    public void PopCardFromHand(int index = -1)
+    public void PopCardFromHand(XRSocketInteractor eventSocket = null)
     {
-        Debug.Log("Card detached from hand.");
+        Debug.Log("Card detached from socket " + eventSocket + ".");
+        Debug.Log(socketCardPairs);
+        var foundTuple = socketCardPairs.Find(x => x.Item1 == eventSocket);
+        socketCardPairs.Remove(foundTuple);
+        Debug.Log("Event socket pair: " + foundTuple);
+        heldCards.Remove(foundTuple.Item2);
+        activeSockets.Remove(eventSocket);
+        inactiveSockets.Push(eventSocket);
+        eventSocket.gameObject.SetActive(false);
+
+        MakeCardFan();
+
+        Debug.Log("You now have " + heldCards.Count + " cards in your hand.");
     }
     
     public void MakeCardFan()
