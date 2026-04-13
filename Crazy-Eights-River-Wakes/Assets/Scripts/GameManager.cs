728504458a9b3a27ea8406300dc1709e24241dab
Richard Chase Mooney <rchasemooney@gmail.com>
2026-04-12 21:46:33 -0400

    automatically determine character order maybe

--- Crazy-Eights-River-Wakes/Assets/Scripts/GameManager.cs
+++ Crazy-Eights-River-Wakes/Assets/Scripts/GameManager.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -18,7 +21,31 @@ public class GameManager : MonoBehaviour
     public List<BaseCharacter> BuildCharactersArray()
     {
         var charactersArray = FindObjectsByType<BaseCharacter>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
-        return new List<BaseCharacter>(charactersArray);
+        var centerPos = GetCenter(charactersArray.Select(c=>c.transform).ToList());
+        // attempt to make order of array match the circular order by sorting by angle from center
+        List<BaseCharacter> charactersList = charactersArray.OrderBy(c =>
+        {
+            Vector3 centerToCharacter = c.transform.position - centerPos;
+            float angle = Mathf.Atan2(centerToCharacter.z, centerToCharacter.x);
+            return angle;
+        }).ToList();
+        return charactersList;
+    }
+
+    // Get center of all the characters
+    private static Vector3 GetCenter(List<Transform> transforms)
+    {
+        if (transforms.Count == 0)
+            return Vector3.zero;
+
+        Vector3 sum = Vector3.zero;
+
+        foreach (Transform t in transforms)
+        {
+            sum += t.position;
+        }
+
+        return sum / transforms.Count;
     }
 }
 
